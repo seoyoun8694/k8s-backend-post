7756f2c2818e85931edd9dc92670c840364105a8
seoyoun8694 <chaeseoyoun8694@gmail.com>
2025-06-17 14:40:01 +0900

    .

--- src/main/java/com/welab/k8s_backend_post/K8sBackendPostApplication.java
+++ src/main/java/com/welab/k8s_backend_post/K8sBackendPostApplication.java
@@ -2,6 +2,7 @@ package com.welab.k8s_backend_post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class K8sBackendPostApplication {
